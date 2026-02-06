cba562a2058c4540d906460f7b5e2452e16b41f4
aastha456 <tamrakaraastha7@gmail.com>
2026-02-06 16:03:17 +0545

    Add logo in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,18 @@ import "./Header.css"
 interface HeaderProps {
     title: string;
     description: string;
+    logo?: string;
 }
 
-const Header = ({title, description}: HeaderProps) => {
+const Header = ({title, description, logo}: HeaderProps) => {
     return (
         <header className="header-container">
+            {logo && <img src={logo} alt="Logo" className="header-logo" />}
+            <div className="header-text">
             <h1 className="header__title">{title}</h1>
             <p className="header__description">{description}</p>
+            </div>
+            
         </header>
     )
 }
